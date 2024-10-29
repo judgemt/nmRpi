7171dfb7721d0175dc0854fef2e4dcf9f3b1fe98
judgemt <judgemt@uga.edu>
2024-10-29 16:11:57 -0400

    driver is working as expected. tested new hardware config (single breadboard). pump functions using stepper commands. need to test pump module.

--- python/test_driver.py
+++ python/test_driver.py
@@ -1,5 +1,6 @@
 from drivers.stepper import A4988
 import RPi.GPIO as GPIO
+import time
 
 # Define pin mappings for motor control
 
@@ -17,11 +18,28 @@ stepper = A4988(pin_mappings=pin_map, auto_calibrate=True, speed=2, pulseWidth=5
 
 # Move without pre-setting the step mode
 # stepper.move(revolutions=4, stepMode="sixteenth", direction="CW", speed=1)
-# stepper.move(revolutions=2, stepMode="half", direction="CCW", speed=2)
-# stepper.move(revolutions=2, stepMode="quarter", direction="CW", speed=2)
-# stepper.move(revolutions=2, stepMode="eighth", direction="CCW", speed=2)
-# stepper.move(steps=10000, stepMode="sixteenth", direction="CCW", speed=1)
-stepper.move(revolutions=4, stepMode="sixteenth", direction="CCW", speed=1)
+# stepper.move(revolutions=2, stepMode="half", direction="CCW", speed=2)1
+# stepper.move(revolutions=2, stepMode="quarter", direction="CW", speed=2)\
+
+time.sleep(5)
+try:
+    for i in range(3):
+        stepper.move(revolutions=4, stepMode="sixteenth", direction="CW", speed=2)
+        time.sleep(1)
+        # stepper.move(steps=10000, stepMode="sixteenth", direction="CCW", speed=1)
+        stepper.move(revolutions=4, stepMode="sixteenth", direction="CCW", speed=2)
+        time.sleep(1)
+        print(i)
+
+except KeyboardInterrupt():
+    print('Done.')
+    stepper.disable()
 
 # Cleanup GPIO
 # stepper.cleanup()
+# stepper.move(revolutions=4, stepMode="sixteenth", direction="CW", speed=1)
+# time.sleep(1)
+# # stepper.move(steps=10000, stepMode="sixteenth", direction="CCW", speed=1)
+# stepper.move(revolutions=4, stepMode="sixteenth", direction="CCW", speed=1)
+
+stepper.disable()
