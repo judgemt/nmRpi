dbe585c343f6adc7f62f2035c9254207c1289084
Michael T. Judge <judgemt@uga.edu>
2025-02-05 17:29:24 -0500

    updating one-pump test script

--- test_pump.py
+++ test_pump.py
@@ -6,14 +6,20 @@ import time
 # Define pin mappings for motor control - don't forget to check config/pin_map.json
 
 # Initialize stepper with auto-calibration
-stepper = A4988(config_file='config/pin_map.json', auto_calibrate=True, speed=2, pulseWidth=5E-6)
+stepper = A4988(config_file='config/pin_map.json', enable_pin=37, auto_calibrate=True, speed=2, pulseWidth=5E-6)
 
-# Initialize pump object globally (could make this session-specific later)
- 
+# Initialize pump object globally
 pump = Pump(motor=stepper, syringe_volume=4, ml_per_rotation=4.5/5, step_mode='sixteenth')
+
+# Enable the pump before use
+pump.enable()
 
 pump.print_info()
 
 pump.move_volume(21, speed=.5)
 
 pump.print_info()
+
+# Clean up
+pump.disable()
+stepper.cleanup()
